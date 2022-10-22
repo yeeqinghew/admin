ccfa75570568529d7f47126c7a32b3335a882c9a
unknown <yeeqinghew@gmail.com>
2022-10-22 17:57:28 +0800

    feat: joinedQuest table

--- src/pages/Quests/QuestHome.js
+++ src/pages/Quests/QuestHome.js
@@ -8,6 +8,7 @@ import Header from "../../components/Header";
 
 const QuestHome = () => {
   const [data, setData] = useState({});
+  const [joinedQuestData, setJoinedQuestData] = useState({});
   const [activeTab, setActiveTab] = useState("Quest");
 
   useEffect(() => {
@@ -17,6 +18,37 @@ const QuestHome = () => {
       } else {
         setData({});
       }
+    });
+
+    // retrieve from JoinedQuests
+    fireDb.child("JoinedQuests").on("value", (snapshot) => {
+      const joinedQuests = snapshot.val();
+      Object.entries(joinedQuests).forEach(([key, val]) => {
+        // get quest's details by questID
+        fireDb
+          .child(`Quest/${val.questId}`)
+          .get()
+          .then((quest) => {
+            if (quest.exists()) {
+              joinedQuests[key] = { ...joinedQuests[key], quest: quest.val() };
+            }
+          });
+        // get merchant's detail by merchantID
+        fireDb
+          .child(`Merchants/${val.merchantId}`)
+          .get()
+          .then((merchant) => {
+            if (merchant.exists()) {
+              joinedQuests[key] = {
+                ...joinedQuests[key],
+                merchant: merchant.val(),
+              };
+            }
+          });
+      });
+
+      setJoinedQuestData(joinedQuests);
+      return;
     });
 
     return () => {
@@ -38,60 +70,96 @@ const QuestHome = () => {
 
   return (
     <>
-    <Header />
-    <br />
-    <br />
-    <Link to="/addQuest">
-      <button className="btn btn-edit">
-        <p
-          className={`${activeTab === "AddQuest" ? "active" : ""}`}
-          onClick={() => setActiveTab("AddQuest")}
-        >
-          Add Quest
-        </p>
+      <Header />
+      <br />
+      <br />
+      <Link to="/addQuest">
+        <button className="btn btn-edit">
+          <p
+            className={`${activeTab === "AddQuest" ? "active" : ""}`}
+            onClick={() => setActiveTab("AddQuest")}
+          >
+            Add Quest
+          </p>
         </button>
       </Link>
-    <div style={{ marginTop: "50px" }}>
-      <table className="styled-table">
-        <thead>
-          <tr>
-            <th style={{ textAlign: "center" }}>Quest ID</th>
-            <th style={{ textAlign: "center" }}>Goal Title</th>
-            <th style={{ textAlign: "center" }}>Quest Title</th>
-            <th style={{ textAlign: "center" }}>Description</th>
-            <th style={{ textAlign: "center" }}>Quest Points</th>
-            <th style={{ textAlign: "center" }}>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {Object.keys(data).map((id, index) => {
-            return (
-              <tr key={id}>
-                <th>{data[id].questId}</th>
-                <td>{data[id].goalTitle}</td>
-                <td>{data[id].questTitle}</td>
-                <td>{data[id].description}</td>
-                <td>{data[id].points}</td>
-                <td>
-                  <Link to={`/updateQuests/${id}`}>
-                    <button className="btn btn-edit">Edit</button>
-                  </Link>
-                  <button
-                    className="btn btn-delete"
-                    onClick={() => onDelete(id)}
-                  >
-                    Delete
-                  </button>
-                  <Link to={`/viewQuests/${id}`}>
-                    <button className="btn btn-view">View</button>
-                  </Link>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
+      <div style={{ marginTop: "50px" }}>
+        <table className="styled-table">
+          <thead>
+            <tr>
+              <th style={{ textAlign: "center" }}>Quest ID</th>
+              <th style={{ textAlign: "center" }}>Goal Title</th>
+              <th style={{ textAlign: "center" }}>Quest Title</th>
+              <th style={{ textAlign: "center" }}>Description</th>
+              <th style={{ textAlign: "center" }}>Quest Points</th>
+              <th style={{ textAlign: "center" }}>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Object.keys(data).map((id, index) => {
+              return (
+                <tr key={id}>
+                  <th>{data[id].questId}</th>
+                  <td>{data[id].goalTitle}</td>
+                  <td>{data[id].questTitle}</td>
+                  <td>{data[id].description}</td>
+                  <td>{data[id].points}</td>
+                  <td>
+                    <Link to={`/updateQuests/${id}`}>
+                      <button className="btn btn-edit">Edit</button>
+                    </Link>
+                    <button
+                      className="btn btn-delete"
+                      onClick={() => onDelete(id)}
+                    >
+                      Delete
+                    </button>
+                    <Link to={`/viewQuests/${id}`}>
+                      <button className="btn btn-view">View</button>
+                    </Link>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+
+        <h3
+          className="Joined Quest"
+          style={{ textAlign: "left", marginLeft: "160px" }}
+        >
+          Joined Quest
+        </h3>
+        <table className="styled-table">
+          <thead>
+            <tr>
+              <th style={{ textAlign: "center" }}>Quest ID</th>
+              <th style={{ textAlign: "center" }}>Quest Title</th>
+              <th style={{ textAlign: "center" }}>Merchant ID</th>
+              <th style={{ textAlign: "center" }}>Company Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {joinedQuestData &&
+              Object.keys(joinedQuestData).map((id, index) => {
+                return (
+                  <tr key={id}>
+                    <th>{joinedQuestData[id].questId}</th>
+                    <td>
+                      {joinedQuestData[id].quest &&
+                        joinedQuestData[id].quest["questTitle"]}
+                    </td>
+                    <td>{joinedQuestData[id].merchantId}</td>
+                    <td>
+                      {joinedQuestData[id].merchant &&
+                        joinedQuestData[id].merchant["companyName"]}
+                    </td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 };
